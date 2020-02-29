0e10226382ed78a322d2db9ee2d47a02b7f06837
MaxMelnik1311 <maaxfresh1100@gmail.com>
2020-02-29 19:15:50 +0200

    Updates

---
+++ precache-manifest.3632f5a0e28761977bd1836c31412557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740ee909ea217a51193c37d2585f9973",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "32ad32619c15a4c65622",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.96384899.chunk.css"
+  },
+  {
+    "revision": "ea30e0f2f12b83a1ff15",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.8bc77883.chunk.js"
+  },
+  {
+    "revision": "32ad32619c15a4c65622",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c66ccfb6.chunk.js"
+  },
+  {
+    "revision": "e2488e0010ad5bdbd4f5",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.eebb98cf.js"
+  }
+]);
